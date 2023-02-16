5902c45a65a5b9ba2678a600c6cf5720bbd8aac8
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-16 12:23:32 -0800

    Get the positivity and negativity scores for each diary entry

---
+++ main.py
@@ -0,0 +1,15 @@
+import glob
+import streamlit as st
+import plotly.express as px
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+filepaths = sorted(glob.glob("diary/*.txt"))
+analyzer = SentimentIntensityAnalyzer()
+negativity = []
+positivity = []
+
+for filepath in filepaths:
+    with open(filepath) as file:
+        content = file.read()
+    scores = analyzer.polarity_scores(content)
+    print(scores)
