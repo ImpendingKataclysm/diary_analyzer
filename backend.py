89f0da34d78b97066b2962cebf041299be9d59e4
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-17 11:46:36 -0800

    downloaded vader lexicon

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import glob
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-nltk.download('vader_-lexicon')
+nltk.downloader.download('vader_-lexicon')
 
 directory = "diary/"
 extension = ".txt"
