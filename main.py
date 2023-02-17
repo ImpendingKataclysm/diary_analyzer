5688bf9188f48e6b625720a0a54bb32a344ae0a2
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-17 09:59:08 -0800

    Created Positivity graph

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import plotly.express as px
 from backend import pos_scores, neg_scores, dates
 
 st.title("Diary Sentiment Analysis")
+st.subheader("Positivity")
+
+figure = px.line(x=dates, y=pos_scores, labels={"x": "Date",
+                                                "y": "Positivity Score"})
+st.plotly_chart(figure)
